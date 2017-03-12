92b43a945b660e1a304cbc6b42b45897f6c71119
taichi-iki <taichi.iki@nextremer.com>
2017-03-13 03:50:11 +0900

    added BruteforceLearner

--- src/learners/sample_learners.py
+++ src/learners/sample_learners.py
@@ -13,6 +13,100 @@ import random
 from core.serializer import StandardSerializer, IdentitySerializer
 from learners.base import BaseLearner
 
+import string, copy
+
+class BruteforceLearner(BaseLearner):
+    def __init__(self):
+        # self.windows_charcode_ignore_on()
+        code_space = list(string.printable)
+        print('initialized code space length=%d'%(len(code_space)), code_space)
+        self.code_space = code_space
+        self.last_reward = 0
+        self.policy_list = [
+                PolicyCorrectRepeat(self.code_space),
+                PolicyMappingSearch(self.code_space),
+                PolicyRandom(self.code_space),
+            ]
+        self.current_policy_id = 0
+    
+    def windows_charcode_ignore_on(self):
+        import sys, codecs
+        writer = codecs.getwriter(sys.stdout.encoding)(sys.stdout, errors='ignore')
+        sys.stdout = writer
+    
+    def reward(self, reward):
+        self.last_reward = reward
+
+    def next(self, input):
+        current_policy = self.policy_list[self.current_policy_id]
+        if current_policy.expired:
+            self.current_policy_id += 1
+            if self.current_policy_id >= len(self.policy_list):
+                self.current_policy_id = 0
+            current_policy = self.policy_list[self.current_policy_id]
+        output = current_policy.next(self.last_reward, input)
+        print('current_policy_id', self.current_policy_id, 'last_reward', self.last_reward, 'input', input, 'output', output)
+        return output
+        
+class PolicyCorrectRepeat(object):
+    def __init__(self, code_space):
+        self.expired = False
+        self.code_space = code_space
+        self.current_pos = 0
+        self.last_correct_pos = -1
+    
+    def next(self, reward, input):
+        if reward == 1:
+            self.last_correct_pos = self.current_pos 
+        else:
+            self.current_pos = (self.current_pos + 1) % len(self.code_space)
+            if self.last_correct_pos == self.current_pos:
+                self.expired = True
+        
+        return self.code_space[self.current_pos]
+        
+class PolicyMappingSearch(object):
+    def __init__(self, code_space):
+        self.expired = False
+        self.code_space = code_space
+        self.map_candidate = {}
+        for x in code_space:
+            self.map_candidate[x] = copy.copy(code_space)
+        self.map_found = {}
+        self.last_pair = None
+    
+    def next(self, reward, input):
+        if (not self.last_pair is None) and reward == 1:
+            last_input, last_output = self.last_pair
+            if not last_input in self.map_found:
+                self.map_found[last_input] = last_output
+                self.map_candidate[last_input] = []
+        if input in self.map_found:
+            output = self.map_found[input]
+        else:
+            if len(self.map_candidate[input]) > 0:
+                output = self.map_candidate[input].pop()
+            else:
+                output = random.choice(self.code_space)
+        left_candidate = 0
+        for l in self.map_candidate.values():
+            left_candidate += len(l)
+        if left_candidate == 0:
+            self.expired = True
+        self.last_pair = (input, output)
+        return output
+
+class PolicyRandom(object):
+    def __init__(self, code_space, max_time=5000):
+        self.expired = False
+        self.code_space = code_space
+        self.left_time = max_time
+    
+    def next(self, reward, input):
+        self.left_time -= 1
+        if self.left_time <= 0:
+            self.expired = True
+        return random.choice(self.code_space)
 
 class SampleRepeatingLearner(BaseLearner):
     def reward(self, reward):
